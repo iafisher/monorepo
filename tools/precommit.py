52879157df55afd883f3599e146e1b7e977af57b
Ian Fisher <iafisher@protonmail.com>
2020-02-20 22:32:15 -0800

    Don't display full list of fix commands in precommit output
    Instead, it just tells you to run mn fix.

--- tools/precommit.py
+++ tools/precommit.py
@@ -16,8 +16,6 @@ def main():
         for problem in problems:
             print_problem(problem)
 
-        print(f"\n{red(plural(len(problems), 'issue'))} detected.", file=sys.stderr)
-
         fixable_problems = [problem for problem in problems if problem.fixable]
         if fixable_problems:
             msg = f"Fix {len(fixable_problems)} of {plural(len(problems), 'issue')}"
@@ -25,23 +23,9 @@ def main():
                 msg = green(msg)
             else:
                 msg = blue(msg)
-
             print(file=sys.stderr)
-            print(f"{msg} with:", file=sys.stderr)
+            print(f"{msg} with {blue('mn fix')}", file=sys.stderr)
 
-            paths_to_fix = set()
-            for problem in fixable_problems:
-                paths_to_fix.add(problem.path)
-                # The only time that problem.fix_command should be falsey when
-                # problem.fixable is True is for unstaged changes to a staged file,
-                # because that problem is solved by the 'git add' command which must be
-                # applied at the end.
-                if problem.fix_command:
-                    print(f"    {problem.fix_command} && \\", file=sys.stderr)
-
-            # 'git add' all the files that the fixes changed.
-            paths_to_fix_as_str = " ".join(map(repr, sorted(paths_to_fix)))
-            print(f"    git add {paths_to_fix_as_str}")
         sys.exit(1)
     else:
         print(f"\n{green('No issues')} detected.", file=sys.stderr)
